523761e479ab2e0b1b768a000fbe0655849d355c
osman28tr <osmantonbul2001@gmail.com>
2022-08-17 20:01:57 +0300

    Kullanıcı default kısmında feature statistics view component'e taşındı.

---
+++ TraversalCoreProje/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Statistics:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var context = new Context();
+            ViewBag.deger1 = context.Destinations.Count();
+            ViewBag.deger2 = context.Guides.Count();
+            ViewBag.deger3 = "285";
+            return View();
+        }
+    }
+}
