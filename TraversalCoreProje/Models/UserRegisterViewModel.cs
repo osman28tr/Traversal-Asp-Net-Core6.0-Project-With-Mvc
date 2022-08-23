c9bdb070e36c4edd2c1f684d443dd27ce5d1f61e
osman28tr <osmantonbul2001@gmail.com>
2022-08-23 12:47:03 +0300

    Authentication yapılandırması yapıldı ve register işlemi gerçekleştirildi.

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen adinizi giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen soyadinizi giriniz")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen kullanici adinizi giriniz")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
+        [Compare("Password", ErrorMessage = "Şifreler uyumlu değil")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- TraversalCoreProje/Program.cs
+++ TraversalCoreProje/Program.cs
@@ -1,17 +1,31 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser()
+    .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddMvc();
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
