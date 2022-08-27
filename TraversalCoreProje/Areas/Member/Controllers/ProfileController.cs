ba210ae5587c83b74cccb367fcee293bf2f1736d
osman28tr <osmantonbul2001@gmail.com>
2022-08-27 20:34:34 +0300

    Sisteme giriş yapan kullanıcının bilgilerini güncelleme işlemi gerçekleştirildi.

--- TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -19,11 +19,36 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             UserEditViewModel userEditViewModel = new UserEditViewModel();
-            userEditViewModel.Name=values.Name;
-            userEditViewModel.Surname=values.Surname;
-            userEditViewModel.PhoneNumber=values.PhoneNumber;
+            userEditViewModel.Name = values.Name;
+            userEditViewModel.Surname = values.Surname;
+            userEditViewModel.PhoneNumber = values.PhoneNumber;
             userEditViewModel.Mail = values.Email;
             return View(userEditViewModel);
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (userEditViewModel.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(userEditViewModel.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/UserImages/" + imageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await userEditViewModel.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            user.Name=userEditViewModel.Name;
+            user.Surname = userEditViewModel.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditViewModel.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            return View();
+        }
     }
 }
+//şifre: 123456aA** ece
