54106585eed3c4053bd9878dcd05267437088fea
osman28tr <osmantonbul2001@gmail.com>
2022-08-31 13:00:44 +0300

    Kullanıcı dashboard kısmında tur rehberleri listeleme işlemi gerçekleştirildi.

---
+++ BusinessLayer/Concrete/GuideManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        IGuideDal _guideDal;
+        public GuideManager(IGuideDal guideDal)
+        {
+            _guideDal = guideDal;
+        }
+
+        public Guide GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guide> TGetList()
+        {
+            return _guideDal.GetList();
+        }
+
+        public void TInsert(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
